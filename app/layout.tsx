660828fe944a9495a5bfbb1f1fc4a66a2bf4f6a0
Balogun Oluwapelumi Johnson <77397898+PJBalogun@users.noreply.github.com>
2025-02-17 10:34:44 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,8 @@ import Footer from "@/components/global/Footer";
 import Navbar from "@/components/global/Navbar";
 import "./globals.css";
 
-
-
 export const metadata: Metadata = {
-  title: "Balogun  OLuwapelumi Portfolio",
+  title: "Balogun Oluwapelumi Portfolio",
   description: "Check Out my information",
 };
 
@@ -16,22 +14,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="text-[70%] sm:text-[75%] md:text-[100%]">
-      <body className={``}>
-						<main className="w-full  flex flex-col items-center justify-center md:min-h-screen text-white "
-							style={{ maxWidth: "1440px" }}>
-							<div className="p-5 w-full flex-1 text-center bg-bg">
-								<div className="relative">
-									<Navbar />
-								</div>
-								<div className="m-5 block sm:hidden z-100">
-									{/* <MobileNavbar /> */}
-								</div>
-								{children}
-							</div>
-						</main>
-						<Footer />
-					</body>
+    <html lang="en" className="text-[70%] sm:text-[75%] md:text-[100%] overflow-x-hidden">
+      <body className="overflow-x-hidden w-full">
+        <main className="w-full flex flex-col items-center justify-center md:min-h-screen text-white overflow-hidden"
+          style={{ maxWidth: "1440px" }}>
+          <div className="p-5 w-full flex-1 text-center bg-bg overflow-hidden">
+            <div className="relative">
+              <Navbar />
+            </div>
+            <div className="m-5 block sm:hidden z-100">
+              {/* <MobileNavbar /> */}
+            </div>
+            <div className="max-w-full w-full">
+              {children}
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </body>
     </html>
   );
 }
